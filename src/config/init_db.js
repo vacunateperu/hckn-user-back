f876f213107bf3d8a2e5a9f5eb11249f8c1d375f
carlosVM <carlos_vm_7@hotmail.com>
2021-02-15 13:44:49 -0500

    config enviroment

--- src/config/init_db.js
+++ src/config/init_db.js
@@ -1,12 +1,6 @@
 const { Pool, Client } = require('pg')
 const dotenv = require("dotenv");
 dotenv.config({ path: '.env'})
-console.log(process.env.PGHOST)
-console.log(process.env.PGDATABASE)
-console.log(process.env.PGPORT)
-console.log(process.env.PGUSER)
-console.log(process.env.PGPASSWORD)
-console.log(process.env.SSL)
 const pool = new Pool({
     host: `${process.env.PGHOST}`,
     database: `${process.env.PGDATABASE}`,
