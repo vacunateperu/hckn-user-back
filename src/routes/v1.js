1fe93c31ec09532b881d1d89edf77ff993400409
carlosVM <carlos_vm_7@hotmail.com>
2021-03-06 17:18:51 -0500

    route identity

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -1,5 +1,6 @@
 const routes = require("express").Router()
-const { query } = require("../controllers/query_CTRL")
+const { query, identity } = require("../controllers/query_CTRL")
 routes.get("/query", query)
 routes.post("/query", query)
+routes.post("/identity", identity)
 module.exports = routes;
