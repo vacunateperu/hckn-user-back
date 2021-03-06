048f24bfe6ec59ea249ab3a98ca2c4ee98a5cfb9
carlosVM <carlos_vm_7@hotmail.com>
2021-03-06 17:14:53 -0500

    route identity

--- src/controllers/query_CTRL.js
+++ src/controllers/query_CTRL.js
@@ -73,6 +73,30 @@ module.exports = {
         } catch (error) {
             next(error)
         }
+    },
+
+    async identity(req, res, next){
+        try {
+            const doc = await DocumentValidator.validateAsync(req.query)
+            const registro = await Read(doc.dni);
+            if(registro.rows .length < 1) {
+                const poll = await PollValidator.validateAsync(req.body)
+                const result = await fetch(`https://apiperu.dev/api/dni/${doc.dni}`, 
+                                { method: 'GET', 
+                                headers: { 
+                                    'Content-Type': 'application/json', 
+                                    'Authorization': `Bearer ${process.env.KEY_API_PERU_DEV}` 
+                                } 
+                                }).then(res => res.json())
+                if(!result.success)  return next(handlerError.BadRequest(result.message))
+                 
+                    res.status(200).send({user: result.data});
+            } else {
+                res.status(200).send(response(registro.rows[0]));
+            }
+        } catch (error) {
+            next(error)
+        }
     }
 }
 
